adc1866c204f3bc88cccced0891c6d57174c5ec9
giannurwana19 <giannurwana19@gmail.com>
2021-06-23 11:15:58 +0700

    membuat tampilan admin dashboard

---
+++ resources/views/layouts/admin/includes/sidebar.blade.php
@@ -0,0 +1,88 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-store-alt"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">R. Block</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Tampilan
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-store"></i>
+            <span>Informai Toko</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="far fa-images"></i>
+            <span>Slider</span></a>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Manajemen Produk
+    </div>
+
+    {{-- Nav Item - Category --}}
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-list"></i>
+            <span>Kategori</span></a>
+    </li>
+
+    <!-- Nav Item - Brand -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="far fa-bookmark"></i>
+            <span>Merk</span></a>
+    </li>
+
+    {{-- Nav Item - Product --}}
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-box-open"></i>
+            <span>Produk</span></a>
+    </li>
+
+
+    <!-- Nav Item - Logout -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Logout</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.frontend.app');
+});
+
+Route::prefix('admina')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index']);
+});
+
+Route::fallback(function () {
+    abort(404);
 });
